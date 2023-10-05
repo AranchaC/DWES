ff522fdc01dd91d9e4b8fc5abfee0dd58c5b9c22
unknown <aranchachicharro@gmail.com>
2023-10-05 21:32:33 +0200

    blog casa 05oct 21.32

---
+++ php/blogFake.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+    <h1></h1>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <label for="nombre">Escribe mensaje: </label>
+            <input type="text" name="texto" required >
+            <input type="submit" name="enviar" value="Enviar" />
+            <br/><br/>
+            <textarea readonly name="area" id="" cols="30" rows="10"></textarea>
+
+
+            <input type="hidden" name="numMaquina" value="<?php echo $numMaquina; ?>">
+            <input type="hidden" name="contador" value="<?php echo $contador; ?>">
+ 
+        </form>
+
+    </body>
+    
+</html>
