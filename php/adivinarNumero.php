407a8fd793e472272638c7b66730e01a0f6e9ff0
AranchaC <aranchachicharro@gmail.com>
2023-10-09 14:05:07 +0200

    fin num aleatorio php

--- php/adivinarNumero.php
+++ php/adivinarNumero.php
@@ -8,47 +8,45 @@
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
             <label for="nombre">Numero: </label>
             <input type="number" name="num" min="1" max="100" required >
+
+            <input type="submit" name="adivinar" value="Adivinar" />
+
+            <?php 
+
+                $numMaquina = isset($_POST["numMaquina"]) ? $_POST["numMaquina"] : rand(1, 100);
+                $contador = isset($_POST["contador"]) ? $_POST["contador"] : 0;
+
+                // $numMaquina = rand(1,100);
+                //$numMaquina = 12;
+                // $contador = 0;
+                
+                if ( isset($_REQUEST["adivinar"])){
+                    $num = $_REQUEST["num"];
+
+                    if ($num == $numMaquina){
+                        $contador++;
+                        echo "<br> Tu número " . $num . " es correcto<br>¡HAS GANADO!<br>";
+                        echo "<br> Intentos: " . $contador . "<br>";
+                        echo "<br> Introduce otro número para volver a jugar.";
+                        $contador = 0;
+                        $numMaquina = rand(1, 100);
+
+                    } elseif ($num > $numMaquina){
+                        $contador++;
+                        echo "<br> Tu número " . $num . " es mayor.<br>";
+                        echo "<br> Intentos: " . $contador . "<br>";                   
+                    } else {
+                        $contador++;
+                        echo "<br> Tu número " . $num . " es menor.<br>";
+                        echo "<br> Intentos: " . $contador . "<br>";
+                    }
+                }
+            ?>
             <input type="hidden" name="numMaquina" value="<?php echo $numMaquina; ?>">
             <input type="hidden" name="contador" value="<?php echo $contador; ?>">
-            <input type="submit" name="adivinar" value="Adivinar" />
         </form>
 
-    </body>
-    
+</body>
+
 </html>
-
-    <?php 
-        session_start();
-        if (!isset($_SESSION["contador"])){
-            $_SESSION["contador"] = 0;
-        }
-        if (!isset($_SESSION["numMaquina"])){
-            $_SESSION["numMaquina"] = rand(1,100);
-        }
-        // $numMaquina = rand(1,100);
-        //$numMaquina = 12;
-        // $contador = 0;
-        
-        if ( isset($_REQUEST["adivinar"])){
-            $num = $_REQUEST["num"];
-            $numMaquina = $_SESSION["numMaquina"];
-
-            if ($num == $numMaquina){
-                $_SESSION["contador"]++;
-                echo "<br> Tu número " . $num . " es correcto<br>¡HAS GANADO!<br>";
-                echo "<br> Intentos: " . $_SESSION["contador"] . "<br>";
-                $_SESSION["contador"] = 0;
-                $_SESSION["numMaquina"] = rand(1,100);
-
-            } elseif ($num > $numMaquina){
-                $_SESSION["contador"]++;
-                echo "<br> Tu número " . $num . " es mayor.<br>";
-                echo "<br> Intentos: " . $_SESSION["contador"] . "<br>";                   
-            } else {
-                $_SESSION["contador"]++;
-                echo "<br> Tu número " . $num . " es menor.<br>";
-                echo "<br> Intentos: " . $_SESSION["contador"] . "<br>";
-            }
-        }
-    ?>
 
---
+++ php/adivinarNumeroSession.php
@@ -0,0 +1,54 @@
+
+<html>
+    <head>
+
+    </head>
+    <body>
+    <h1>Adivina un numero del 1 al 100</h1>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <label for="nombre">Numero: </label>
+            <input type="number" name="num" min="1" max="100" required >
+            <input type="hidden" name="numMaquina" value="<?php echo $numMaquina; ?>">
+            <input type="hidden" name="contador" value="<?php echo $contador; ?>">
+            <input type="submit" name="adivinar" value="Adivinar" />
+        </form>
+
+    </body>
+    
+</html>
+
+    <?php 
+        session_start();
+        if (!isset($_SESSION["contador"])){
+            $_SESSION["contador"] = 0;
+        }
+        if (!isset($_SESSION["numMaquina"])){
+            $_SESSION["numMaquina"] = rand(1,100);
+        }
+        // $numMaquina = rand(1,100);
+        //$numMaquina = 12;
+        // $contador = 0;
+        
+        if ( isset($_REQUEST["adivinar"])){
+            $num = $_REQUEST["num"];
+            $numMaquina = $_SESSION["numMaquina"];
+
+            if ($num == $numMaquina){
+                $_SESSION["contador"]++;
+                echo "<br> Tu número " . $num . " es correcto<br>¡HAS GANADO!<br>";
+                echo "<br> Intentos: " . $_SESSION["contador"] . "<br>";
+                $_SESSION["contador"] = 0;
+                $_SESSION["numMaquina"] = rand(1,100);
+
+            } elseif ($num > $numMaquina){
+                $_SESSION["contador"]++;
+                echo "<br> Tu número " . $num . " es mayor.<br>";
+                echo "<br> Intentos: " . $_SESSION["contador"] . "<br>";                   
+            } else {
+                $_SESSION["contador"]++;
+                echo "<br> Tu número " . $num . " es menor.<br>";
+                echo "<br> Intentos: " . $_SESSION["contador"] . "<br>";
+            }
+        }
+    ?>
+
