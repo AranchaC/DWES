83b20998078937057453b782b95bff0faace31ad
AranchaC <aranchachicharro@gmail.com>
2023-12-22 08:29:05 +0100

    clase 22dec 08.29

---
+++ ValidacionTut3/src/main/java/com/example/demo/ValidacionTut3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidacionTut3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidacionTut3Application.class, args);
+	}
+
+}
