35861a78636ce667659663c682aa9e2d95971c0a
AranchaC <aranchachicharro@gmail.com>
2024-01-16 18:04:06 +0100

    casa 16jan 18.04

---
+++ Reto4-UD4/D4_UD4_QUIZ/src/main/java/com/example/demo/Resultado.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Resultado {
+    private Clasificacion clasificacion;
+    private int puntos;
+
+    public Clasificacion getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(Clasificacion clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+}
